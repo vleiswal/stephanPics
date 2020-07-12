29b2346bc016b34a4c517a0e1f6c00a349565929
Vleis Walker <vleiswal@gmail.com>
2020-07-12 13:24:57 +0200

    Starting

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+// import helper code
+import 'package:flutter/material.dart';
+
+// define a main function
+void main() {
+  var app = MaterialApp(
+    home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Hi there');
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+      appBar: AppBar(
+        title: Text('Lets see some images!'),
+      ),
+    ),
+  );
+
+  runApp(app);
+}
